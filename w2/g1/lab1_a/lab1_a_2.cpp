e6eec0eefb907c502704e8c4403d9185b81be127
Beisenbek Baisakov <bbaisakov@dar.kz>
2020-09-12 11:03:56 +0600

    upd

---
+++ w2/g1/lab1_a/lab1_a_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a, b;
+
+    cout << "Vvedite 4islo a:" << endl;
+    cin >> a;
+    cout << "Vvedite 4islo b:" << endl;
+    cin >> b;
+
+    cout << "result:" << endl;
+    cout << a + b;
+
+    return 0;
+}
