aa009c68788cd5acc804f4912488ffea2df70976
Beisenbek Baisakov <bbaisakov@dar.kz>
2020-09-19 14:50:25 +0600

    upd

---
+++ w3/g2/while operator/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int i = 1;
+
+    for(;i <= 10;){
+        cout << i << " ";
+        i = i + 1;
+    }
+
+    int j = 1;
+    while(j <= 10){
+        cout << j << " ";
+        j = j + 1;
+    }
+
+    return 0;
+}
---
+++ w3/g2/while operator/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int j = 1;
+    do{
+        cout << j << " ";
+        j = j + 1;
+    }while(j <= 10);
+
+    return 0;
+}
